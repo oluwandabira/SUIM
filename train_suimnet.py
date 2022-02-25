95934069162c6fc678a4abe4e8145cdab7392efb
Alawode Oluwandabira <dabiraalawode@yahoo.com>
2022-02-25 11:01:37 +0200

    Corrections for modern Tensorflow/Keras

--- train_suimnet.py
+++ train_suimnet.py
@@ -9,29 +9,30 @@ from os.path import join, exists
 from keras import callbacks
 # local libs
 from models.suim_net import SUIM_Net
-from utils.data_utils import trainDataGenerator
+from utils.data import suim_dataset
 
-## dataset directory
+# dataset directory
 dataset_name = "suim"
-train_dir = "/mnt/data1/ImageSeg/suim/train_val/"
+train_dir = "data/train_val/"
 
-## ckpt directory
+# ckpt directory
 ckpt_dir = "ckpt/"
-base_ = 'VGG' # or 'RSB'
-if base_=='RSB':
-    im_res_ = (320, 240, 3) 
+base_ = 'VGG'  # or 'RSB'
+if base_ == 'RSB':
+    im_res_ = (320, 240, 3)
     ckpt_name = "suimnet_rsb.hdf5"
-else: 
+else:
     im_res_ = (320, 256, 3)
     ckpt_name = "suimnet_vgg.hdf5"
 model_ckpt_name = join(ckpt_dir, ckpt_name)
-if not exists(ckpt_dir): os.makedirs(ckpt_dir)
+if not exists(ckpt_dir):
+    os.makedirs(ckpt_dir)
 
-## initialize model
+# initialize model
 suimnet = SUIM_Net(base=base_, im_res=im_res_, n_classes=5)
 model = suimnet.model
-print (model.summary())
-## load saved model
+print(model.summary())
+# load saved model
 #model.load_weights(join("ckpt/saved/", "***.hdf5"))
 
 
@@ -39,32 +40,24 @@ batch_size = 8
 num_epochs = 50
 # setup data generator
 data_gen_args = dict(rotation_range=0.2,
-                    width_shift_range=0.05,
-                    height_shift_range=0.05,
-                    shear_range=0.05,
-                    zoom_range=0.05,
-                    horizontal_flip=True,
-                    fill_mode='nearest')
+                     width_shift_range=0.05,
+                     height_shift_range=0.05,
+                     shear_range=0.05,
+                     zoom_range=0.05,
+                     horizontal_flip=True,
+                     fill_mode='nearest')
 
-model_checkpoint = callbacks.ModelCheckpoint(model_ckpt_name, 
-                                   monitor = 'loss', 
-                                   verbose = 1, mode= 'auto',
-                                   save_weights_only = True,
-                                   save_best_only = True)
+model_checkpoint = callbacks.ModelCheckpoint(model_ckpt_name,
+                                             monitor='loss',
+                                             verbose=1, mode='auto',
+                                             save_weights_only=True,
+                                             save_best_only=True)
 
 # data generator
-train_gen = trainDataGenerator(batch_size, # batch_size 
-                              train_dir,# train-data dir
-                              "images", # image_folder 
-                              "masks", # mask_folder
-                              data_gen_args, # aug_dict
-                              image_color_mode="rgb", 
-                              mask_color_mode="rgb",
-                              target_size = (im_res_[1], im_res_[0]))
+dataset = suim_dataset(train_dir, im_res_[:1])
 
-## fit model
-model.fit_generator(train_gen, 
-                    steps_per_epoch = 5000,
-                    epochs = num_epochs,
-                    callbacks = [model_checkpoint])
-
+# fit model
+model.fit_generator(dataset,
+                    steps_per_epoch=5000,
+                    epochs=num_epochs,
+                    callbacks=[model_checkpoint])
---
+++ utils/data.py
@@ -0,0 +1,58 @@
+from typing import List, Tuple
+from pathlib import Path
+
+import numpy as np
+import tensorflow as tf
+import cv2
+
+categories_code = [
+    "BW", "HD", "PF", "WR",
+    "RO", "RI", "FV", "SR"
+]
+
+
+def get_categories(mask: np.ndarray) -> np.ndarray:
+    imw, imh = mask.shape[0], mask.shape[1]
+
+    categories = np.zeros((imw, imh, 8))
+
+    for i in range(imw):
+        for j in range(imh):
+            code = int("".join([str(i) for i in mask[i, j].tolist()]), 2)
+            categories[i, j, code] = 1
+
+    return categories
+
+
+def list_categories(categories: np.ndarray) -> List[str]:
+    return [cat for code, cat in enumerate(categories_code) if (np.any(categories[:, :, code]))]
+
+
+def generate_images_masks(data_dir: str, img_size: Tuple[int, int]):
+    path = Path(data_dir)
+    images = sorted(path.glob("images/*"))
+    masks = sorted(path.glob("masks/*"))
+
+    for img_file, mask_file in zip(images, masks):
+        img = cv2.imread(str(img_file))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, img_size)
+        img = img / 255
+
+        mask = cv2.imread(str(mask_file))
+        mask = cv2.cvtColor(mask, cv2.COLOR_BGR2RGB)
+        mask = cv2.resize(mask, img_size)
+
+        mask = mask / 255
+        yield img, get_categories(np.uint8(mask))
+
+
+def suim_dataset(data_dir, img_size):
+    img_gen = generate_images_masks(data_dir, img_size)
+    return tf.data.Dataset.from_generator(
+        lambda: (x for x in img_gen),
+        output_signature=(
+            tf.TensorSpec(
+                shape=(img_size[1], img_size[0], 3), dtype=tf.float32),
+            tf.TensorSpec(shape=(img_size[1], img_size[0], 8), dtype=tf.uint8)
+        ))
